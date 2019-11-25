466ccd5355de3d13265d7121c24ce01c95fe590d
Miguel Alatzar <this.migbot@gmail.com>
2019-11-24 19:40:42 -0700

    Remove mapStateToProps

--- app/components/channel_keyboard_accessory/recorder/index.js
+++ app/components/channel_keyboard_accessory/recorder/index.js
@@ -3,17 +3,9 @@
 
 import {connect} from 'react-redux';
 
-import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
-
 import {createVoiceMessage} from 'app/actions/views/voice';
 
 import Recorder from './recorder';
-
-function mapStateToProps(state) {
-    return {
-        theme: getTheme(state),
-    };
-};
 
 const mapDispatchToProps = {
     createVoiceMessage,
