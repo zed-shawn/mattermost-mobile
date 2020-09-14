551916741d5c9c92cca4c2e2625e4603b76730ef
Miguel Alatzar <this.migbot@gmail.com>
2020-09-14 14:11:18 -0700

    Use Fontello config and ttf

--- app/components/compass_icon/index.js
+++ app/components/compass_icon/index.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {createIconSetFromIcoMoon} from 'react-native-vector-icons';
-import icoMoonConfig from '@assets/compass-icons.json';
+import {createIconSetFromFontello} from 'react-native-vector-icons';
+import fontelloConfig from '@assets/compass-icons.json';
 
-export default createIconSetFromIcoMoon(icoMoonConfig, 'Compass', 'Compass.ttf');
+export default createIconSetFromFontello(fontelloConfig, 'compass-icons', 'compass-icons.ttf');
